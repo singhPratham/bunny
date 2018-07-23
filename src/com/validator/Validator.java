3b88f2c5e39fc591050cd7b3b1442fa417b47522
Avruti <avruti.sri.1609@gmail.com>
2018-07-22 23:51:36 -0700

    validator commit

--- src/com/validator/Validator.java
+++ src/com/validator/Validator.java
@@ -2,7 +2,23 @@ package com.validator;
 
 public class Validator {
 
-    public boolean validate (final String query) {
-
-    }
+	public boolean validate(final String query) {
+		if (query == null) {
+			return false;
+		}
+		if (query.length() != 5) {
+			return false;
+		}
+		final String[] temp = query.split(" ");
+		try {
+			Integer.parseInt(temp[0]);
+			Integer.parseInt(temp[2]);
+		} catch (Exception ex) {
+			return false;
+		}
+		if (temp[1].equals("+") || temp[1].equals("-") || temp[1].equals("/") || temp[1].equals("*")) {
+			return true;
+		}
+		return false;
+	}
 }
