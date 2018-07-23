d7ec26c242b44ed64eed1e723d5966303d816070
singhPratham <prathamwork2012@gmail.com>
2018-07-23 03:56:19 -0400

    added ValidatorTest

--- tst/com/validator/ValidatorTest.java
+++ tst/com/validator/ValidatorTest.java
@@ -1,5 +1,18 @@
 package com.validator;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import com.validator.Validator;
+
 public class ValidatorTest {
 
+    private Validator validator = new Validator();
+
+    @Test
+    public void testValidator() {
+        final String query = "3 + 2";
+        final boolean result = validator.validate(query);
+        assertTrue(result);
+    }
 }
